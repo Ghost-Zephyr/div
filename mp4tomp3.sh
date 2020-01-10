9f53ed9cd0fe504c47dcbff018ad2130a71ef234
Sivert <siv@localhost.localdomain>
2020-01-10 15:09:53 +0100

    crime

---
+++ mp4tomp3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# converts all mp4 files in a directory to mp3
+if [ $# -eq 0 ]; then
+	FOLDER=$(pwd)
+else
+	FOLDER=$1
+fi
+MP4FILES=$(ls $FOLDER | grep .mp4)
+FILE=''
+for l in $MP4FILES
+do
+	case $l in
+		*.mp4)
+			FILE+=' '$l
+			NFILE=${FILE%.mp4}.mp3
+			ffmpeg -i "$FOLDER/$FILE" "$FOLDER/$NFILE" # -b:a 192K -vn
+			FILE=''
+		;;
+		*)
+			if [ -z "$FILE" ]; then
+				FILE=$l
+			else
+				FILE+=' '$l
+			fi
+		;;
+	esac
+done
+
